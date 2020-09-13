d64243506b99732d0c3af738d92e67b9f79a6373
Andre <info@pcgames.lt>
2020-09-13 20:15:34 +0300

    Закончена стилизация шапки

---
+++ source/js/burger.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// Переключатель меню
+
+// var headerNav = document.querySelector('.header__nav');
+// var logo = document.querySelector('.logo');
+
+(function () {
+  var burger = document.querySelector('.burger');
+
+  function toggleBurger(action) { // action = {open, close}
+    if (action === 'open') {
+      if (!(burger.classList.contains('burger--open'))) {
+        burger.classList.add('burger--open');
+      }
+    }
+
+    if (action === 'close') {
+      if (burger.classList.contains('burger--close')) {
+        burger.classList.remove('burger--open');
+      }
+    }
+  }
+
+  window.burger = {
+    init: function () {
+      toggleBurger('close');
+    }
+  };
+})();
+
+// function toggleMenu(action) { // action = {hide, show}
+//     if (action === 'hide' ) {
+//         if (headerNav.classList.contains('header__nav--tablet-visible')) {
+//             headerNav.classList.remove('header__nav--tablet-visible')
+//         }
+//     }
+
+//     if (action === 'show' ) {
+//         if (!headerNav.classList.contains('header__nav--tablet-visible')) {
+//             headerNav.classList.add('header__nav--tablet-visible')
+//         }
+//     }
+// }
+
+// function toggleLogo(action) { // action = {hide, show}
+//     if (action === 'hide' ) {
+//         if (!logo.classList.contains('logo--hidden')) {
+//             logo.classList.add('logo--hidden')
+//         }
+//     }
+
+//     if (action === 'show' ) {
+//         if (logo.classList.contains('logo--hidden')) {
+//             logo.classList.remove('logo--hidden');
+//         }
+//     }
+// }
